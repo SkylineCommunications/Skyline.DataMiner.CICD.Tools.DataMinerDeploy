ebc0b4ddddfc6a06efe2e535cffa902914be8619
Jan Staelens <jan.staelens@skyline.be>
2023-12-13 15:44:45 +0100

    Rename Artifacts folder to  DataMinerArtifacts

---
+++ Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/DataMinerArtifacts/CatalogArtifact.cs
@@ -0,0 +1,168 @@
+﻿namespace Skyline.DataMiner.CICD.Tools.DataMinerDeploy.Lib
+{
+	using System;
+	using System.Runtime.InteropServices;
+	using System.Threading;
+	using System.Threading.Tasks;
+
+	using Microsoft.Extensions.Logging;
+
+	using Newtonsoft.Json;
+
+	internal class CatalogArtifact : IArtifact
+	{
+		private readonly ILogger _logger;
+		private readonly string artifactIdentifier;
+		private readonly CancellationTokenSource cancellationTokenSource;
+		private readonly string catalogAgentToken;
+		private readonly ICatalogService service;
+		private string keyFromEnv;
+
+		public CatalogArtifact(ICatalogService service, string artifactIdentifier, string catalogAgentToken, ILogger logger)
+		{
+			this.artifactIdentifier = artifactIdentifier;
+			this.catalogAgentToken = catalogAgentToken;
+			this._logger = logger;
+			this.service = service;
+			cancellationTokenSource = new CancellationTokenSource();
+		}
+
+		public CatalogArtifact(string artifactIdentifier, string catalogAgentToken, ILogger logger) : this(CatalogServiceFactory.CreateWithHttp(new System.Net.Http.HttpClient(), logger), artifactIdentifier, catalogAgentToken, logger)
+		{
+		}
+
+		public CatalogArtifact(ICatalogService service, string artifactIdentifier, ILogger logger)
+		{
+			cancellationTokenSource = new CancellationTokenSource();
+			this.artifactIdentifier = artifactIdentifier;
+			this._logger = logger;
+			this.service = service;
+			TryFindEnvironmentKey();
+			if (String.IsNullOrWhiteSpace(keyFromEnv))
+			{
+				throw new InvalidOperationException("Deployment failed, missing token in environment variable dmcatalogtoken or dmcatalogtoken_encrypted. Either add that, or use the provided catalogAgentToken argument.");
+			}
+
+			catalogAgentToken = keyFromEnv;
+		}
+
+		public CatalogArtifact(string artifactIdentifier, ILogger logger) : this(CatalogServiceFactory.CreateWithHttp(new System.Net.Http.HttpClient(), logger), artifactIdentifier, logger)
+		{
+		}
+
+		/// <summary>
+		/// Cancels an ongoing deployment. Create a new CatalogArtifact to attempt a new upload.
+		/// </summary>
+		public void CancelDeployment()
+		{
+			_logger.LogDebug($"Upload cancellation requested for {artifactIdentifier}");
+			cancellationTokenSource.Cancel();
+		}
+
+		public async Task<bool> DeployAsync(TimeSpan timeout)
+		{
+			var deploying = await service.DeployPackageAsync(artifactIdentifier, catalogAgentToken, cancellationTokenSource.Token);
+			if (deploying == null)
+			{
+				throw new InvalidOperationException($"Start Deployment of {artifactIdentifier} failed. CatalogService returned a null");
+			}
+
+			_logger.LogDebug("Deployment Started...");
+			var output = await ConfirmSuccesfullDeploymentAsync(catalogAgentToken, TimeSpan.FromSeconds(3), TimeSpan.FromMinutes(2), timeout, deploying);
+			_logger.LogDebug("Deployment Finished.");
+			_logger.LogInformation(JsonConvert.SerializeObject(output));
+			return true;
+		}
+
+		private async Task<DeployedPackage> ConfirmSuccesfullDeploymentAsync(string key, TimeSpan deploymentBackOff, TimeSpan deploymentMaxBackOff,
+			TimeSpan deploymentTimeout, DeployingPackage deployingPackage)
+		{
+			DeployedPackage deployedPackage;
+			var maxTimeout = deploymentTimeout.TotalSeconds;
+			_logger.LogDebug($"Waiting {((int)deploymentBackOff.TotalSeconds)}s before checking deployment status.");
+			await Task.Delay((int)deploymentBackOff.TotalMilliseconds);
+
+			try
+			{
+				deployedPackage = await Utils.ExecuteWithRetryAsync(
+					async () =>
+					{
+						if (cancellationTokenSource.IsCancellationRequested)
+						{
+							throw new OperationCanceledException();
+						}
+
+						return await service.GetDeployedPackageAsync(deployingPackage, key);
+					},
+					(output) => (output?.Status ?? "").Equals("succeeded", StringComparison.InvariantCultureIgnoreCase) || (output?.Status ?? "").Equals("Timeout", StringComparison.InvariantCultureIgnoreCase) || (output?.Status ?? "").Equals("Error", StringComparison.InvariantCultureIgnoreCase),
+					(backOffDelaySeconds) => _logger.LogDebug($"Waiting {backOffDelaySeconds}s before checking next deployment status."),
+					deploymentBackOff,
+					deploymentMaxBackOff,
+					deploymentTimeout.Subtract(deploymentBackOff));
+			}
+			catch (TimeoutException ex)
+			{
+				throw new TimeoutException($"Deployment Status was never succeeded, error or timeout after trying for {maxTimeout} seconds");
+			}
+
+			if (deployedPackage.Status.Equals("Timeout", StringComparison.InvariantCultureIgnoreCase) || deployedPackage.Status.Equals("Error", StringComparison.InvariantCultureIgnoreCase))
+			{
+				_logger.LogCritical($"Deployment Failed with status {deployedPackage.Status} for artifact {deployingPackage.ArtifactId}");
+				return null;
+			}
+
+			return deployedPackage;
+		}
+
+		/// <summary>
+		///  Attempts to find the necessary API key in Environment Variables. In order of priority:
+		///  <para>- key stored as an Environment Variable called "dmcatalogtoken". (unix/win)</para>
+		///  <para>- key configured using Skyline.DataMiner.CICD.Tools.WinEncryptedKeys called "dmcatalogtoken_encrypted" (windows only)</para>
+		/// </summary>
+		private void TryFindEnvironmentKey()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				try
+				{
+					var encryptedKey = WinEncryptedKeys.Lib.Keys.RetrieveKey("dmcatalogtoken_encrypted");
+					if (encryptedKey != null)
+					{
+						string keyFromWinEncryptedKeys = new System.Net.NetworkCredential(string.Empty, encryptedKey).Password;
+
+						if (!String.IsNullOrWhiteSpace(keyFromWinEncryptedKeys))
+						{
+							_logger.LogDebug("OK: Found token in Env Variable: 'dmcatalogtoken_encrypted' created by WinEncryptedKeys.");
+							keyFromEnv = keyFromWinEncryptedKeys;
+						}
+					}
+				}
+				catch (InvalidOperationException)
+				{
+					// Gobble up, no key means we try the next thing.
+				}
+			}
+
+			//var config = new ConfigurationBuilder()
+			//	.AddUserSecrets<CatalogArtifact>()
+			//	.Build();
+			//string keyFromEnvironment = config["dmcatalogtoken"];
+
+			string keyFromEnvironment = Environment.GetEnvironmentVariable("dmcatalogtoken");
+
+			if (!String.IsNullOrWhiteSpace(keyFromEnvironment))
+			{
+				if (!String.IsNullOrWhiteSpace(keyFromEnv))
+				{
+					_logger.LogDebug("OK: Overriding 'dmcatalogtoken_encrypted' with found token in Env Variable: 'dmcatalogtoken'.");
+				}
+				else
+				{
+					_logger.LogDebug("OK: Found token in Env Variable: 'dmcatalogtoken'.");
+				}
+
+				keyFromEnv = keyFromEnvironment;
+			}
+		}
+	}
+}
---
+++ Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/DataMinerArtifacts/IArtifact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Skyline.DataMiner.CICD.Tools.DataMinerDeploy.Lib
+{
+	/// <summary>
+	/// Represents an artifact you wish to deploy to a DataMiner system.
+	/// </summary>
+	public interface IArtifact
+	{
+		/// <summary>
+		/// Attempts to cancel an ongoing deployment. Create a new IArtifact to attempt a new deployment.
+		/// </summary>
+		/// <returns></returns>
+		void CancelDeployment();
+
+		/// <summary>
+		/// Deploys and artifact to the system and waits until timeout for deployment.
+		/// </summary>
+		/// <returns>True if deployment was successful.</returns>
+		Task<bool> DeployAsync(TimeSpan timeout);
+	}
+}
