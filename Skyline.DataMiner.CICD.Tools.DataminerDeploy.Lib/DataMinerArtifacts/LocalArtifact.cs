6e0d0118e4d7be2cc964d9ccf663148dcffe3148
Jan Staelens <jan.staelens@skyline.be>
2023-12-13 17:17:52 +0100

    WIP

--- Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/DataMinerArtifacts/LocalArtifact.cs
+++ Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/DataMinerArtifacts/LocalArtifact.cs
@@ -33,7 +33,7 @@
 		{
 		}
 
-		public LocalArtifact(string pathToArtifact, string dataMinerServerLocation, string dataminerUser, string dataminerPassword, ILogger logger) : this(new SLNetDataMinerService("TODO"), pathToArtifact, dataMinerServerLocation, dataminerUser, dataminerPassword, logger)
+		public LocalArtifact(string pathToArtifact, string dataMinerServerLocation, string dataminerUser, string dataminerPassword, ILogger logger)
 		{
 		}
 
@@ -65,6 +65,87 @@
 			}
 
 			return true;
+		}
+
+		/// <summary>
+		///  Attempts to find the necessary API key in Environment Variables. In order of priority:
+		///  <para>- key stored as an Environment Variable called "dmcatalogtoken". (unix/win)</para>
+		///  <para>- key configured using Skyline.DataMiner.CICD.Tools.WinEncryptedKeys called "dmcatalogtoken_encrypted" (windows only)</para>
+		/// </summary>
+		private void TryFindEnvironmentKeys()
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				try
+				{
+					var encryptedKey = WinEncryptedKeys.Lib.Keys.RetrieveKey("dmusername_encrypted");
+					if (encryptedKey != null)
+					{
+						string keyFromWinEncryptedKeys = new System.Net.NetworkCredential(string.Empty, encryptedKey).Password;
+
+						if (!String.IsNullOrWhiteSpace(keyFromWinEncryptedKeys))
+						{
+							_logger.LogDebug("OK: Found token in Env Variable: 'dmusername_encrypted' created by WinEncryptedKeys.");
+							userFromEnv = keyFromWinEncryptedKeys;
+						}
+					}
+				}
+				catch (InvalidOperationException)
+				{
+					// Gobble up, no key means we try the next thing.
+				}
+
+				try
+				{
+					var encryptedKey = WinEncryptedKeys.Lib.Keys.RetrieveKey("dmpassword_encrypted");
+					if (encryptedKey != null)
+					{
+						string keyFromWinEncryptedKeys = new System.Net.NetworkCredential(string.Empty, encryptedKey).Password;
+
+						if (!String.IsNullOrWhiteSpace(keyFromWinEncryptedKeys))
+						{
+							_logger.LogDebug("OK: Found token in Env Variable: 'dmpassword_encrypted' created by WinEncryptedKeys.");
+							pwFromEnv = keyFromWinEncryptedKeys;
+						}
+					}
+				}
+				catch (InvalidOperationException)
+				{
+					// Gobble up, no key means we try the next thing.
+				}
+			}
+
+			string userFromEnvironment = Environment.GetEnvironmentVariable("dmusername");
+
+			if (!String.IsNullOrWhiteSpace(userFromEnvironment))
+			{
+				if (!String.IsNullOrWhiteSpace(userFromEnv))
+				{
+					_logger.LogDebug("OK: Overriding 'dmusername_encrypted' with found token in Env Variable: 'dmusername'.");
+				}
+				else
+				{
+					_logger.LogDebug("OK: Found token in Env Variable: 'dmusername'.");
+				}
+
+				userFromEnv = userFromEnvironment;
+			}
+
+			string pwFromEnvironment = Environment.GetEnvironmentVariable("dmpassword");
+
+			if (!String.IsNullOrWhiteSpace(pwFromEnvironment))
+			{
+				if (!String.IsNullOrWhiteSpace(pwFromEnv))
+				{
+					_logger.LogDebug("OK: Overriding 'dmpassword_encrypted' with found token in Env Variable: 'dmpassword'.");
+				}
+				else
+				{
+					_logger.LogDebug("OK: Found token in Env Variable: 'dmpassword'.");
+				}
+
+				pwFromEnv = pwFromEnvironment;
+			}
 		}
 
 		/// <summary>
