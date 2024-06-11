a30b06932ae719c25e1d7be07754cef92126d662
Michiel Oda <97458010+MichielOda@users.noreply.github.com>
2024-06-11 08:47:59 +0200

    Add try catch with better error message (#21)

--- Skyline.DataMiner.CICD.Tools.DataMinerDeploy.Lib/DataMinerService/SLNet/SLNetCommunication.cs
+++ Skyline.DataMiner.CICD.Tools.DataMinerDeploy.Lib/DataMinerService/SLNet/SLNetCommunication.cs
@@ -25,7 +25,18 @@
             // Going to need to make a GRPC Connection directly. Remoting does NOT work on .NET6
             // Minimum Supported DataMiner is then: Main Release 10.3.0   february 2023   feature release 10.3.2
 
-            Connection = new GRPCConnection(hostname);
+            try
+            {
+                Connection = new GRPCConnection(hostname);
+            }
+            catch (Exception ex)
+            {
+                // DataMinerOfflineException when APIGateway is not present or too low version. (Tested on DM 10.1 & 10.2)
+                // Keeping it a generic catch to deal with possible differences between DM versions. Error should be clearer instead of an unclear error from DataMiner.
+
+                throw new InvalidOperationException("Unable to reach DataMiner. Make sure that DataMiner and APIGateway are up and running and DataMiner has a minimum version of MR 10.3 / FR 10.3.2 ", ex);
+            }
+
             Connection.PollingRequestTimeout = 120000;
             Connection.ConnectTimeoutTime = 120000;
             Connection.AuthenticateMessageTimeout = 120000;
