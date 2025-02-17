364aefbf2d48ba946d4744ae7a3e74493f74f2b2
Jan Staelens <jan.staelens@skyline.be>
2025-02-17 14:43:02 +0100

    Added XML documentation.

--- Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/CatalogService/KeyCatalogDeploymentIdentifier.cs
+++ Skyline.DataMiner.CICD.Tools.DataminerDeploy.Lib/CatalogService/KeyCatalogDeploymentIdentifier.cs
@@ -1,13 +1,23 @@
 ﻿namespace Skyline.DataMiner.CICD.Tools.DataMinerDeploy.Lib.CatalogService
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
-
+    /// <summary>
+    /// Represents a unique identifier for a deployment in the DataMiner Catalog.
+    /// </summary>
     public class KeyCatalogDeploymentIdentifier
     {
+        /// <summary>
+        /// Gets or sets the globally unique identifier (GUID) of the catalog entry.
+        /// </summary>
         public string CatalogGuid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of the catalog entry.
+        /// </summary>
         public string CatalogVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the globally unique identifier (GUID) of the deployment destination.
+        /// </summary>
         public string DestinationGuid { get; set; }
     }
 }
